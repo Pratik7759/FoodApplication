f241cff6c90d54bc3167a77996b472346e36a0f9
Pratik7759 <pratik682019@gmail.com>
2023-04-03 12:27:46 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent{
   title = 'FoodApplication';
+  // allowNewServer = false;
+  // status = 'why choose uhd';
+  // serverName='a';
+  // username="";
+  // constructor() {
+  //   setTimeout(() => {
+  //     this.allowNewServer = true;
+  //   }, 2000);
+  // }
+
+  // onChangeStatus() {
+  //   this.status = 'criminal justice';
+  // }
+
+  // onUpdateServerName(event: Event) {
+  //   // console.log(event.target.value);
+  //   this.serverName= (<HTMLInputElement>event.target).value;
+  //   console.log(this.serverName);
+  // }
+
+
+  
+
 }
